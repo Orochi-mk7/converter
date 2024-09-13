d6221dd4bda1db7c4b20e586ca020f99d4ed8632
Orochi-mk7 <139254765+Orochi-mk7@users.noreply.github.com>
2024-09-13 14:33:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const lenght = document.getElementById("lenght")
+const volume = document.getElementById("volume")
+const mass = document.getElementById("mass")
+const unit = document.getElementById("unit")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function() {
+    let toBeConverted = unit.value
+    let meterToFeet = toBeConverted * 3.281
+    let feetToMeter = toBeConverted / 3.281
+    let literToGallon = toBeConverted * 0.264
+    let GallonToLiter = toBeConverted / 0.264
+    let kiloToPound = toBeConverted * 2.204
+    let poundToKilo = toBeConverted / 2.204
+    lenght.textContent = `${toBeConverted} meters = ${meterToFeet.toFixed(3)} feet | 
+    ${toBeConverted} feet = ${feetToMeter.toFixed(3)} meters`
+
+    volume.textContent = `${toBeConverted} liters = ${literToGallon.toFixed(3)} gallons | 
+    ${toBeConverted} gallons = ${GallonToLiter.toFixed(3)} liters`
+
+    mass.textContent = `${toBeConverted} kilos = ${kiloToPound.toFixed(3)} pounds | 
+    ${toBeConverted} pounds = ${poundToKilo.toFixed(3)} kilos`
+})
+
